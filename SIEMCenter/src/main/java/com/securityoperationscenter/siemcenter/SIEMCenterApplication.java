022bc20898c7b3bda28af2fcf29d34957ef85e5c
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-22 21:12:29 +0200

    Initialize Spring Boot application for SIEM Center

---
+++ SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/SIEMCenterApplication.java
@@ -0,0 +1,13 @@
+package com.securityoperationscenter.siemcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIEMCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIEMCenterApplication.class, args);
+	}
+
+}
