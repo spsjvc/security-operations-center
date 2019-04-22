ff12976fdc8344437d0076a564d7b9524149305d
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-22 23:28:37 +0200

    Add test for example rule

--- SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/SIEMCenterApplication.java
+++ SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/SIEMCenterApplication.java
@@ -1,7 +1,11 @@
 package com.securityoperationscenter.siemcenter;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SIEMCenterApplication {
@@ -10,4 +14,19 @@ public class SIEMCenterApplication {
 		SpringApplication.run(SIEMCenterApplication.class, args);
 	}
 
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices kieServices = KieServices.Factory.get();
+		KieContainer kieContainer = kieServices
+			.newKieContainer(
+				kieServices.newReleaseId(
+				"com.securityoperationscenter",
+				"siem-center-rules",
+				"0.0.1-SNAPSHOT"
+				)
+			);
+		KieScanner kieScanner = kieServices.newKieScanner(kieContainer);
+		kieScanner.start(10_000);
+		return kieContainer;
+	}
 }
