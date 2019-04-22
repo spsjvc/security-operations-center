c1992f8f4a7ff31818161249772d2767a43962a8
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-22 23:08:04 +0200

    Add example model

---
+++ SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/model/Example.java
@@ -0,0 +1,10 @@
+package com.securityoperationscenter.siemcenter.model;
+
+public class Example {
+
+    private String content;
+
+    public Example(String content) {
+        this.content = content;
+    }
+}
