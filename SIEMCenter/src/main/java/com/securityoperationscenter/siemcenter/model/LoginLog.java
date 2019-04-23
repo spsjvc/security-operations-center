79e57b003b83dc75ff4f18f62657ce0d14934ef9
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-23 15:03:25 +0200

    Fix successful attribute

--- SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/model/LoginLog.java
+++ SIEMCenter/src/main/java/com/securityoperationscenter/siemcenter/model/LoginLog.java
@@ -5,12 +5,28 @@ import java.util.Date;
 public class LoginLog extends Log {
 
     private String username;
-    private boolean isSuccessful;
+    private boolean successful;
 
-    public LoginLog(Date timestamp, Machine machine, String application, String username, boolean isSuccessful) {
+    public LoginLog(Date timestamp, Machine machine, String application, String username, boolean successful) {
         super(timestamp, machine, application);
 
         this.username = username;
-        this.isSuccessful = isSuccessful;
+        this.successful = successful;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean getSuccessful() {
+        return successful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        this.successful = successful;
     }
 }
