81bbe00f6318468a62197e91da1cb6ab357ec0ba
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-04-23 15:03:46 +0200

    Add rule for three unsuccessful logins with same username

---
+++ SIEMCenter/src/test/java/com/securityoperationscenter/siemcenter/RulesTests.java
@@ -0,0 +1,81 @@
+package com.securityoperationscenter.siemcenter;
+
+import com.securityoperationscenter.siemcenter.model.Example;
+import com.securityoperationscenter.siemcenter.model.LoginLog;
+import com.securityoperationscenter.siemcenter.model.Machine;
+import org.drools.core.base.RuleNameEqualsAgendaFilter;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.AgendaFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RulesTests {
+
+    @Autowired
+    KieContainer kieContainer;
+
+    @Test
+    public void threeUnsuccessfulLoginsWithTheSameUsername() {
+        KieSession kieSession = kieContainer.newKieSession();
+        AgendaFilter filter = new RuleNameEqualsAgendaFilter("Three unsuccessful logins with the same username");
+        int firedRules = kieSession.fireAllRules(filter);
+        Assert.assertEquals(0, firedRules);
+
+        Machine testMachine = new Machine("0.0.0.0", "Windows");
+
+        LoginLog loginLog = new LoginLog(
+            new Date(),
+            testMachine,
+            "testApplication",
+            "testUsername",
+            true
+        );
+
+        kieSession.insert(loginLog);
+        firedRules = kieSession.fireAllRules(filter);
+        Assert.assertEquals(0, firedRules);
+        kieSession.dispose();
+
+        kieSession = kieContainer.newKieSession();
+        loginLog = new LoginLog(
+            new Date(),
+            testMachine,
+            "testApplication",
+            "testUsername",
+            false
+        );
+        kieSession.insert(loginLog);
+
+        loginLog = new LoginLog(
+            new Date(),
+            testMachine,
+            "testApplication",
+            "testUsername",
+            false
+        );
+        kieSession.insert(loginLog);
+
+        loginLog = new LoginLog(
+            new Date(),
+            testMachine,
+            "testApplication",
+            "testUsername",
+            false
+        );
+        kieSession.insert(loginLog);
+
+        firedRules = kieSession.fireAllRules(filter);
+
+        Assert.assertEquals(3, firedRules);
+        kieSession.dispose();
+    }
+}
