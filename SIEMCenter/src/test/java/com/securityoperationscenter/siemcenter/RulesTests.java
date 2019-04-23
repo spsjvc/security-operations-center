523230d4870f59ca1cde60e474c9d41ae4176b88
Mladen Grujicic <mladen.grujicic96@gmail.com>
2019-04-23 15:34:37 +0200

    Add rule for fifteen unsuccessful logins from the same ip

--- SIEMCenter/src/test/java/com/securityoperationscenter/siemcenter/RulesTests.java
+++ SIEMCenter/src/test/java/com/securityoperationscenter/siemcenter/RulesTests.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -76,6 +75,66 @@ public class RulesTests {
         firedRules = kieSession.fireAllRules(filter);
 
         Assert.assertEquals(3, firedRules);
+        kieSession.dispose();
+    }
+
+    @Test
+    public void fifteenUnsuccessfulLoginsFromTheSameIp() {
+        KieSession kieSession = kieContainer.newKieSession();
+        AgendaFilter filter = new RuleNameEqualsAgendaFilter("Fifteen unsuccessful logins from the same ip in five days");
+        int firedRules = kieSession.fireAllRules(filter);
+        Assert.assertEquals(0, firedRules);
+
+        LoginLog loginLog;
+
+        Machine testMachine1 = new Machine("0.0.0.0", "Windows");
+        Machine testMachine2 = new Machine("0.0.0.1", "Windows");
+
+        for (int i = 0; i < 10; i++) {
+            loginLog = new LoginLog(
+                LocalDateTime.now().plusMinutes(i),
+                testMachine1,
+                "testApplication",
+                "testUsername",
+                false
+            );
+
+            kieSession.insert(loginLog);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            loginLog = new LoginLog(
+                    LocalDateTime.now().plusMinutes(i),
+                    testMachine2,
+                    "testApplication",
+                    "testUsername",
+                    false
+            );
+
+            kieSession.insert(loginLog);
+        }
+
+        firedRules = kieSession.fireAllRules(filter);
+        Assert.assertEquals(0, firedRules);
+        kieSession.dispose();
+
+        kieSession = kieContainer.newKieSession();
+
+        for (int i = 0; i < 15; i++) {
+            loginLog = new LoginLog(
+                LocalDateTime.now().plusMinutes(i),
+                testMachine1,
+                "testApplication",
+                "testUsername",
+                false
+            );
+
+            kieSession.insert(loginLog);
+        }
+
+        firedRules = kieSession.fireAllRules(filter);
+        Assert.assertEquals(1, firedRules);
+
         kieSession.dispose();
     }
 }
