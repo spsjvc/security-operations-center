93d5df742cc494e8786bf200f9a43d64e1ccf95a
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-06-19 13:03:55 +0200

    Client

---
+++ siem-center-client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from 'axios'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
+export default function() {
+  return (
+    <Container style={{ height: '100%' }}>
+      <Row style={{ marginTop: '2rem' }}>
+        <h1>Security Operations Center</h1>
+      </Row>
+      <Row style={{ marginTop: '2rem' }}>
+        <Col md={4}>
+          <Button
+            onClick={() => {
+              axios.post('http://localhost:8080/simulate-payment-system-alarm')
+            }}
+          >
+            Simulate Payment System Alarm
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
